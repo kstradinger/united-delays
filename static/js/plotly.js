d9a478d71c0e287ee64ad52771fb4585c685d517
ELLOWE <47371493+ELLowe@users.noreply.github.com>
2019-09-24 18:48:36 -0500

    Added divs for plotly

---
+++ static/js/plotly.js
@@ -0,0 +1,6 @@
+// HEENA Plot the charts to divs tagged by ids: "heenaGraph1", "heenaGraph2", "Heena-side-by-side1", "Heena-side-by-side2"
+// example:
+// Plotly.newPlot("HEENA-plot", data, layout);
+
+
+
